df88cb64008136e7ce0b1f8f578d994c13dbf16d
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-11-26 21:50:00 +0500

    Added accessibility

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,14 +1,31 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  useEffect(() => {
+    const handleEscape = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handleEscape);
+    return () => document.removeEventListener("keydown", handleEscape);
+  }, []);
+
   const isAnyOpen = activeIndex !== null;
+
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, index) => {
         const handleOpen = () => {
           if (activeIndex === index) {

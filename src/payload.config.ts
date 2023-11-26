33f8a9a0085a8a78b12bf32e4423b0684e2c7026
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-11-26 23:50:11 +0500

    connected mongodb with payload and fixed connection of nodemon and express backend server

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,4 +1,8 @@
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { slateEditor } from "@payloadcms/richtext-slate";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { buildConfig } from "payload/config";
+import path from "path";
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
@@ -6,6 +10,22 @@ export default buildConfig({
   routes: {
     admin: "/sell",
   },
-  admin: {},
+  admin: {
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: "- DigiHippo",
+      favicon: "/favicon.ico",
+      ogImage: "/thumbnail.jpg",
+    },
+  },
+  rateLimit: {
+    max: 2000,
+  },
   editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGO_URI || "",
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  },
 });

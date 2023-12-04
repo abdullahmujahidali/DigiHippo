f969900256c3b27bc63f6231a520a0b27ce08a84
abdullahmujahidali <abdullahmujahidali1@gmail.com>
2023-12-04 12:28:32 +0500

    working on verify email

--- src/app/(auth)/verify-email/page.tsx
+++ src/app/(auth)/verify-email/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import VerifyEmail from "@/components/VerifyEmail";
 
 interface PageProps {
   searchParams: { [key: string]: string | string[] | undefined };
@@ -10,7 +11,9 @@ const VerifyEmailPage = ({ searchParams }: PageProps) => {
     <div className="container relative flex pt-20 flex-col items-center justify-center lg:px:0">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         {token && typeof token === "string" ? (
-          <div className="grid gap-6"></div>
+          <div className="grid gap-6">
+            <VerifyEmail token={token} />
+          </div>
         ) : (
           <div className="flex h-full flex-col items-center justify-center space-y-1">
             <div className="relative mb-4 h-60 w-60 text-muted-foreground">
---
+++ src/components/VerifyEmail.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const VerifyEmail = () => {
+  const {} = trpc.auth;
+  return (
+    <div>
+      <h1>Verify Email</h1>
+    </div>
+  );
+};
+
+export default VerifyEmail;
